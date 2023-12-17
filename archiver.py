6e82268d72a8facffc23839fb74f59def0984ca1
BEOM NAMGOONG <tombeom@naver.com>
2023-12-17 23:44:31 +0900

    ✨ feat: 환경설정 추가

---
+++ archiver.py
@@ -0,0 +1,47 @@
+import re
+import json
+from requests import get
+from bs4 import BeautifulSoup
+from math import ceil
+from pathlib import Path
+
+class Archiver():
+    def __init__(self, blog_id):
+        self.BASE_DIR = Path(__file__).resolve().parent
+        self.SETTINGS_FILE_DIR = Path(self.BASE_DIR).joinpath("settings.json")
+        self._check_settings()
+        self.settings = self._load_settings()
+
+        self.blog_id = blog_id
+        self.header = self.settings["SETTINGS"]["HEADERS"]
+        self.total_number_of_posts = None
+        self.posts_list = list()
+        self.image_list = list()
+
+    def _check_settings(self):
+        if not Path(self.SETTINGS_FILE_DIR).exists(): self._create_settings()
+
+    def _create_settings(self):
+        settings = {
+            "SETTINGS": {
+                "DOWNLOAD_DIR" : f"{self.BASE_DIR}",
+                "HEADERS" : {
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+                }
+            }
+        }
+
+        with open(self.SETTINGS_FILE_DIR, 'x') as file:
+            json.dump(settings, file, indent=4, ensure_ascii=False)
+
+    def _load_settings(self):
+        with open(self.SETTINGS_FILE_DIR, 'r') as file:
+            settings = json.load(file)
+
+        return settings
+
+    def change_settings(self, option, data):
+        if option == "DOWNLOAD_DIR":
+            self.settings["SETTINGS"]["DOWNLOAD_DIR"] = data
+        elif option == "HEADERS":
+            self.settings["SETTINGS"]["HEADERS"] = data
